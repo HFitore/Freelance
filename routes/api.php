ac1021739c7f5f8b0fbc8a442d89d6e896eddb02
Taulant Dema <taulantdema12@gmail.com>
2020-06-26 09:31:58 +0200

    added new routes

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,13 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+Route::group([ 'prefix' => 'auth'], function (){
+    Route::group(['middleware' => ['guest:api']], function () {
+        Route::post('login', 'Api\AuthController@login');
+        Route::post('signup', 'Api\AuthController@signup');
+    });
+    Route::group(['middleware' => 'auth:api'], function() {
+        Route::get('logout', 'Api\AuthController@logout');
+        Route::get('getuser', 'Api\AuthController@getUser');
+    });
+});
