2c7146c6e335178d1da879fb06d96471cf3b6ae1
Taulant Dema <taulantdema12@gmail.com>
2020-06-30 13:41:14 +0200

    changes to users table

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -19,10 +19,14 @@ class CreateUsersTable extends Migration
             $table->string('username')->unique();
             $table->string('email')->unique();
             $table->string('password');
+            $table->boolean('active')->default(false);
+            $table->string('activation_token');
             $table->string('image')->nullable();
             $table->unsignedBigInteger('role_id')->nullable();
             $table->rememberToken();
             $table->timestamps();
+            $table->softDeletes();
+
         });
     }
 
